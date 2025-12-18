3761523c05deb56d33b7526deecdc606c86829d6
Shradha Mishra <97448096+parthdeshmukh2@users.noreply.github.com>
2025-12-19 03:43:33 +0530

    theme changes done

--- src/components/layout/Notifications.tsx
+++ src/components/layout/Notifications.tsx
@@ -1,34 +1,40 @@
-import React from 'react';
-import {Box} from "@mui/material";
+import React from "react";
+import { Box } from "@mui/material";
 import Typography from "@mui/material/Typography";
-import {NotificationCard} from "./Notifications/NotificationCard";
-import {COMMENT} from "../../config/customData";
+import { NotificationCard } from "./Notifications/NotificationCard";
+import { COMMENT } from "../../config/customData";
 
 const TITLE = {
-    fontSize: "25px",
-    fontFamily: "Karla",
-    fontWeight: "bold",
-    color: "#F9C336",
-}
-
-export const Notifications = () => {
-    return (
-        <Box>
-
-            <Typography sx={(theme) => (
-                {
-                    ...TITLE,
-                    [theme.breakpoints.down('md')]: {
-                        display: "none",
-                    },
-                }
-            )} >Notifications</Typography>
-            <Box sx={{mt: "0px"}}>
-                {COMMENT.map((comment) => {
-                    return <NotificationCard userImage={comment.userImage} username={comment.username} type={comment.type}/>
-                })}
-            </Box>
-        </Box>
-    );
+  fontSize: "25px",
+  fontFamily: "Karla",
+  fontWeight: "bold",
+  color: "krmrPlate.mainTextColor",
 };
 
+export const Notifications = () => {
+  return (
+    <Box>
+      <Typography
+        sx={(theme) => ({
+          ...TITLE,
+          [theme.breakpoints.down("md")]: {
+            display: "none",
+          },
+        })}
+      >
+        Notifications
+      </Typography>
+      <Box sx={{ mt: "0px" }}>
+        {COMMENT.map((comment) => {
+          return (
+            <NotificationCard
+              userImage={comment.userImage}
+              username={comment.username}
+              type={comment.type}
+            />
+          );
+        })}
+      </Box>
+    </Box>
+  );
+};
